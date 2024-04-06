155b869172a94f9593c7c46fd0b32a1f1f971d2d
hatifmujahid <hatifmujahid08@gmail.com>
2024-04-06 17:06:40 +0500

    sdad

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BackgroundBeams } from "../src/components/ui/background-beams";
 export default function App() {
   return (
     <div className="h-screen w-sreen bg-neutral-950 relative flex flex-col items-center justify-center ">
-    <div className="max-w-2xl mx-auto p-4">
-      <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+    <div className="max-w-4xl mx-auto p-4">
+      <h1 className="relative z-10 text-lg md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
         Developer's Day 2024 Chatbot
       </h1>
       <p></p>
-      <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+      <p className="text-neutral-500 max-w-2xl mx-auto my-2 text-lg text-center relative z-10">
         Welcome to Developer's Day, 2024! <br />
         We are excited to have you here. <br />
         This is a chatbot to help you with all the information you need for the event. <br />
