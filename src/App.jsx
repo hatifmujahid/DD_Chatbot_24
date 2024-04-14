257d541eec9399553e722c6c5f93b8691901a55b
hatifmujahid <hatifmujahid08@gmail.com>
2024-04-14 19:47:34 +0500

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { BackgroundBeams } from "../src/components/ui/background-beams";
+import './App.css';
 export default function App() {
   return (
     <div className="h-screen w-sreen bg-neutral-950 relative flex flex-col items-center justify-center ">
-    <div className="max-w-4xl mx-auto p-4">
-      <h1 className="relative z-10 text-lg md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+      <div className="max-w-4xl mx-auto p-4">
+    <h1 className="relative z-10 text-4xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
         Developer's Day 2024 Chatbot
-      </h1>
-      <p></p>
-      <p className="text-neutral-500 max-w-2xl mx-auto my-2 text-lg text-center relative z-10">
+    </h1>
+    <p></p>
+    <p className="text-neutral-500 max-w-2xl mx-auto my-2 text-lg sm:text-base md:text-2xl text-center relative z-10">
         Welcome to Developer's Day, 2024! <br />
         We are excited to have you here. <br />
         This is a chatbot to help you with all the information you need for the event. <br />
-      </p>
-    </div>
-    <BackgroundBeams />
+        Click on the chat icon below to get started. <br />
+        
+    </p>
   </div>
+      {/* <BackgroundBeams /> */}
+    </div>
   )
 }
