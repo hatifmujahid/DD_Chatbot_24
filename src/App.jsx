056b2b8a6a7dbd13b8bb0ec043b04eb35e16ab56
hatifmujahid <hatifmujahid08@gmail.com>
2024-04-14 20:07:39 +0500

    ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BackgroundBeams } from "../src/components/ui/background-beams";
 import './App.css';
+import Navbar from "./Nav";
 export default function App() {
   return (
     <div className="h-screen w-sreen bg-neutral-950 relative flex flex-col items-center justify-center ">
+      <Navbar />
       <div className="max-w-4xl mx-auto p-4">
     <h1 className="relative z-10 text-4xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
         Developer's Day 2024 Chatbot
@@ -14,7 +16,7 @@ export default function App() {
         We are excited to have you here. <br />
         This is a chatbot to help you with all the information you need for the event. <br />
         Click on the chat icon below to get started. <br />
-        
+
     </p>
   </div>
       {/* <BackgroundBeams /> */}
---
+++ src/Nav.jsx
@@ -0,0 +1,196 @@
+import React, { useState, useEffect } from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+import logo from './assets/logoIcon.png'
+import { motion, AnimatePresence, MotionConfig } from 'framer-motion'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const [bgColor, setBgColor] = useState('transparent')
+    const [lastScrollY, setLastScrollY] = useState(0)
+    const [showNavbar, setShowNavbar] = useState(true)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY
+
+        if (currentScrollY > 50) {
+            setBgColor('#095e7488') // Change to your desired color when scrolled
+        } else {
+            setBgColor('transparent')
+        }
+
+        if (currentScrollY > lastScrollY) {
+            // Scrolling down
+            setShowNavbar(false)
+        } else {
+            // Scrolling up
+            setShowNavbar(true)
+        }
+
+        setLastScrollY(currentScrollY)
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [lastScrollY])
+
+    const navItems = [
+        { id: 1, text: 'Home', href: 'https://acmdevday.com/' },
+        { id: 2, text: 'About Us', href: 'https://acmdevday.com/about' },
+        { id: 3, text: 'Register', href: 'https://acmdevday.com/register' },
+        {id: 4, text: 'ChatBot', href: 'https://chatbot.acmdevday.com'},
+        {id: 5, text: 'FYPXtreme', href: 'https://fypxtreme.acmdevday.com'},
+    ]
+
+    return (
+        <div
+            className={`fixed w-full flex justify-between items-center h-16 mx-auto px-6 md:px-12 z-50 transition-all duration-300 ${
+                showNavbar ? 'top-0' : '-top-20'
+            }`}
+            style={{ backgroundColor: bgColor }}
+        >
+            <a href="/">
+                <img src={logo} alt="logo" className="w-16 z-12" />
+            </a>
+
+            <ul className="hidden md:flex">
+                {navItems.map((item) => (
+                    <li
+                        key={item.id}
+                        className={item.text !== "ChatBot" ? "text-white text-xl border-b-0 hover:border-b-2 hover:text-[#23B6DF] duration-200 ease-in delay-75 p-2 mx-4 cursor-pointer" : "rounded-xl border-b border-[#23B6DF] px-4 text-white text-xl p-2 mx-4 cursor-pointer hover:bg-gradient-to-r hover:from-[#23B6DF] hover:to-[#088097]"}
+                    >
+                        <a href={item.href}>{item.text}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="block md:hidden z-10">
+                <motion.button
+                    initial="hide"
+                    animate={nav ? 'show' : 'hide'}
+                    onClick={handleNav}
+                    className="flex flex-col space-y-1 relative z-10"
+                >
+                    <motion.span
+                        variants={{
+                            hide: {
+                                rotate: 0,
+                            },
+                            show: {
+                                rotate: 45,
+                                y: 9,
+                            },
+                        }}
+                        className="w-8 bg-white h-1 block rounded-full"
+                    ></motion.span>
+                    <motion.span
+                        variants={{
+                            hide: {
+                                opacity: 1,
+                            },
+                            show: {
+                                opacity: 0,
+                            },
+                        }}
+                        className="w-8 bg-white h-1 block rounded-full"
+                    ></motion.span>
+                    <motion.span
+                        variants={{
+                            hide: {
+                                rotate: 0,
+                            },
+                            show: {
+                                rotate: -45,
+                                y: -6,
+                            },
+                        }}
+                        className="w-8 bg-white h-1 block rounded-full"
+                    ></motion.span>
+                </motion.button>
+            </div>
+
+            {/* Mobile Navigation Menu */}
+            <AnimatePresence>
+                {nav && (
+                    <MotionConfig
+                        transition={{
+                            type: 'spring',
+                            bounce: 0.2,
+                        }}
+                    >
+                        <motion.div
+                            key="mobile-nav"
+                            variants={{
+                                hide: {
+                                    x: '100%',
+                                    transition: {
+                                        type: 'spring',
+                                        bounce: 0.2,
+                                        when: 'afterChildren',
+                                        staggerChildren: 0.1, // Reduced stagger delay
+                                    },
+                                },
+                                show: {
+                                    x: '0%',
+                                    transition: {
+                                        type: 'spring',
+                                        bounce: 0.2,
+                                        when: 'beforeChildren',
+                                        staggerChildren: 0.1, // Reduced stagger delay
+                                    },
+                                },
+                            }}
+                            initial="hide"
+                            animate="show"
+                            exit="hide"
+                            className="fixed inset-0 bg-[#0445558a] backdrop-blur-xl p-6 flex flex-col justify-center space-y-10 lg:hidden"
+                        >
+                            <motion.ul
+                                variants={{
+                                    hide: {
+                                        y: '25%',
+                                        opacity: 0,
+                                    },
+                                    show: {
+                                        y: '0%',
+                                        opacity: 1,
+                                    },
+                                }}
+                                className="list-none space-y-6"
+                            >
+                                {navItems.map((item) => (
+                                    <motion.li
+                                        key={item.id}
+                                        variants={{
+                                            hide: { opacity: 0, y: -20 },
+                                            show: { opacity: 1, y: 0 },
+                                        }}
+                                        initial="hide"
+                                        animate="show"
+                                        exit="hide"
+                                    >
+                                        <a
+                                            href={item.href}
+                                            className="text-5xl font-semibold text-white"
+                                        >
+                                            {item.text}
+                                        </a>
+                                    </motion.li>
+                                ))}
+                            </motion.ul>
+                        </motion.div>
+                    </MotionConfig>
+                )}
+            </AnimatePresence>
+        </div>
+    )
+}
+
+export default Navbar
