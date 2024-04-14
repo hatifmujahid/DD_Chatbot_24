f9b800240ad0677f1b9b6b4c180d163dc335dc62
Muhammad Hatif Mujahid <56687118+hatifmujahid@users.noreply.github.com>
2024-04-15 00:53:48 +0500

    Create Footer.jsx

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,16 @@
+import react from "react"
+
+const Footer = () => {
+    return (
+        <div className="bg-[#1a1a1a] text-white p-4 text-center">
+            <div className="text-xl font-bold">
+                ACM DevDay 2024
+            </div>
+            <div className="text-md">
+                Developed by <a href="https://github.com/SameerVers3">Sameer Ahmed</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
